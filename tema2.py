e287b7642ea584bff8c9ba4a19e3cc6d05c9e218
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2018-12-06 23:55:37 +0200

    trying to generate all parents states for probabilities

--- tema2.py
+++ tema2.py
@@ -1,10 +1,11 @@
 import parser
 from node import Node, Factor
 from graph import Graph
+from itertools import product
 
 
 def build_bayesian_graph(bayesian_vars):
-    Phi = Factor([], {})
+    Phi = []
     # for bayesian_var in bayesian_vars:
     #     # print(list(map(lambda s: s.strip(), bayesian_var.split(";"))))
     #     name, parents, probs = list(map(lambda s: s.strip(), bayesian_var.split(";")))
@@ -12,6 +13,19 @@ def build_bayesian_graph(bayesian_vars):
     #     probs = [float(x) for x in probs.split()]
     #     print(name + "|" + str(parents) + "|" + str(probs))
     print(bayesian_vars)
+    for bayesian_var in bayesian_vars:
+        name, parents, probs = bayesian_var
+        phi = Factor([], {})
+        phi.vars.append(name)
+        for parent in parents:
+            phi.vars.append(parent)  # Check if order matters
+            print(parent)
+        parents = ["X","Y","Z"]
+        for stuff in product(range(2), repeat=len(parents)):
+            print(stuff)
+            print(list(zip(parents, stuff)))
+        stuff = [list(zip(parents, stuff2)) for stuff2 in product(range(2), repeat=len(parents))]
+        print(stuff)  # List of lists with zips
 
 
 
